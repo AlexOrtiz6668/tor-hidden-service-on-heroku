13b40312a471e754dd1e16fd7c48b600797ccf76
Alex Ortiz <89742348+AlexOrtiz6668@users.noreply.github.com>
2021-12-22 13:39:11 -0500

    Update index.php

--- public_html/index.php
+++ public_html/index.php
@@ -2,7 +2,7 @@
           <?php
           if (file_exists(__DIR__.'/../temp/var/lib/tor/hidden_service/hostname')) {
               $output = file_get_contents(__DIR__.'/../temp/var/lib/tor/hidden_service/hostname');
-              echo "<a href=http://$output/Home/>$output</a>/Home/";
+              echo "<a href=http://$output/Home/>type in $output</a>/Home/ for the site with no spaces";
           } else {
               $output = "No domain";
               echo '<p>Something went wrong<br>Refer to  Github Repository <br><a href="https://github.com/sumithemmadi/tor-hidden-service-on-heroku#readme">https://github.com/sumithemmadi/tor-hidden-service-on-heroku#readme</a></p>';
