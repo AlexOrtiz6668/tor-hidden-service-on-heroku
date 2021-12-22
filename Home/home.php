10d5ea56e32cd971fe6b204eca628fb89a7e82a5
Alex Ortiz <89742348+AlexOrtiz6668@users.noreply.github.com>
2021-12-22 13:03:46 -0500

    Add files via upload

---
+++ Home/home.php
@@ -0,0 +1,48 @@
+<?php
+// Password protect this content
+require_once('protect-this.php');
+?>
+<?php
+   if(isset($_FILES['image'])){
+      $errors= array();
+      $file_name = $_FILES['image']['name'];
+      $file_size = $_FILES['image']['size'];
+      $file_tmp = $_FILES['image']['tmp_name'];
+      $file_type = $_FILES['image']['type'];
+      $file_ext=strtolower(end(explode('.',$_FILES['image']['name'])));
+      
+      $extensions= array("torrent");
+      
+      if(in_array($file_ext,$extensions)=== false){
+         $errors[]="extension not allowed, please choose a Torrent File.";
+      }
+      
+      if($file_size > 2097152) {
+         $errors[]='File size must be excately 2 MB';
+      }
+      
+      if(empty($errors)==true) {
+         move_uploaded_file($file_tmp,"Storage/".$file_name);
+         echo "Success";
+      }else{
+         print_r($errors);
+      }
+   }
+?>
+ <link rel="stylesheet" href="style.css">
+<title>Tenix Torrent Share</title>
+<html>
+   <body>
+      <center><h1>Upload A Torrent To Our Storage To Host</h1> <br>
+      <a href='https://kimbatt.github.io/torrent-creator/'>we recomend using an online torrent maker for longer hosting </a>
+      <form action = "" method = "POST" enctype = "multipart/form-data">
+         <input type = "file" name = "image" />
+         <input type = "submit"/>
+      </form>
+      </center>
+   </body>
+</html>
+<center>
+<h3>list of Torrents</h3> <br>
+<iframe src="/list.php" height="1000" width="700" title="Our List Of Torrents"></iframe>
+</center>
