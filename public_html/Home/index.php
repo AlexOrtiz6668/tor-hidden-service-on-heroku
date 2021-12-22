4ebd01c83dc9c8be82720e8843f62246cbc5ecb1
Alex Ortiz <89742348+AlexOrtiz6668@users.noreply.github.com>
2021-12-22 13:24:30 -0500

    Update index.php

--- public_html/Home/index.php
+++ public_html/Home/index.php
@@ -1,39 +1,44 @@
 <?php
-    /* Your password */
-    $password = 'oNfW6iGdZe0xoEgqak';
-
-    /* Redirects here after login */
-    $redirect_after_login = 'home.php';
-
-    /* Will not ask password again for */
-    $remember_password = strtotime('+30 days'); // 30 days
-
-    if (isset($_POST['password']) && $_POST['password'] == $password) {
-        setcookie("password", $password, $remember_password);
-        header('Location: ' . $redirect_after_login);
-        exit;
-    }
+   if(isset($_FILES['image'])){
+      $errors= array();
+      $file_name = $_FILES['image']['name'];
+      $file_size = $_FILES['image']['size'];
+      $file_tmp = $_FILES['image']['tmp_name'];
+      $file_type = $_FILES['image']['type'];
+      $file_ext=strtolower(end(explode('.',$_FILES['image']['name'])));
+      
+      $extensions= array("torrent");
+      
+      if(in_array($file_ext,$extensions)=== false){
+         $errors[]="extension not allowed, please choose a Torrent File.";
+      }
+      
+      if($file_size > 2097152) {
+         $errors[]='File size must be excately 2 MB';
+      }
+      
+      if(empty($errors)==true) {
+         move_uploaded_file($file_tmp,"Storage/".$file_name);
+         echo "Success";
+      }else{
+         print_r($errors);
+      }
+   }
 ?>
-
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="utf-8">
-        <link rel="stylesheet" href="style.css">
-    <title>Login</title>
-</head>
-<body>
-    <div id="main">
-                <h1>Login</h1>
-                <div id="login">
-                                                <form method="post" action="/index.php">
-                                <div>Type In The Password To The Website</div>
-                                <div>
-                                <div><input type="password" name="password"></div>
-                                <div><input type="submit" name="login_btn" value="Login"></div>
-                                <div></div>
-                        </form>
-                </div>
-        </div>
-</body>
+ <link rel="stylesheet" href="style.css">
+<title>Tenix Torrent Share</title>
+<html>
+   <body>
+      <center><h1>Upload A Torrent To Our Storage To Host</h1> <br>
+      <a href='https://kimbatt.github.io/torrent-creator/'>we recomend using an online torrent maker for longer hosting </a>
+      <form action = "" method = "POST" enctype = "multipart/form-data">
+         <input type = "file" name = "image" />
+         <input type = "submit"/>
+      </form>
+      </center>
+   </body>
 </html>
+<center>
+<h3>list of Torrents</h3> <br>
+<iframe src="/list.php" height="1000" width="700" title="Our List Of Torrents"></iframe>
+</center>
